87f2c49d098435b596cb10e116bbe999642f179d
anik-biswas <anik.biswas.an@gmail.com>
2023-10-19 21:51:52 -0700

    product delete with delete method successfully

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -33,6 +33,18 @@ async function run() {
     app.get('/product',async(req,res)=>{
         const cursor = productCollection.find();
         const result = await cursor.toArray();
+        res.send(result);
+    })
+    app.delete('/product/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await productCollection.deleteOne(query);
+        res.send(result);
+    })
+    app.get('/brand/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await storeCollection.findOne(query);
         res.send(result);
     })
     app.post('/product',async(req,res)=>{
